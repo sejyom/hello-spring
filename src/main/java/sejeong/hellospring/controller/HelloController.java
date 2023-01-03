4cc232e5a34123e75aacecfdcead0c70f634e607
sejyom <akxxkd01@gmail.com>
2023-01-03 23:33:22 +0900

    domain/repository

--- src/main/java/sejeong/hellospring/controller/HelloController.java
+++ src/main/java/sejeong/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -14,9 +15,33 @@ public class HelloController {
         return "hello";
     }
 
-    @GetMapping("hell-mvc")
-    public String helloMvc(@RequestParam(name = "name") String name, Model model) {
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody // 객체 반환 시 json 으로 변환
+    public Hello HelloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
---
+++ src/main/java/sejeong/hellospring/domain/MemberRepository.java
@@ -0,0 +1,11 @@
+package sejeong.hellospring.domain;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member); // 저장소에 저장
+    Optional<Member> findById(Long id); // 아이디 반환
+    Optional<Member> findByName(String name); // 이름 반환
+    List<Member> findAll(); // 모든 회원 리스트 반환
+}
