2846f77d88503110e5fd691e1bf50276921a1b94
sejyom <akxxkd01@gmail.com>
2023-01-04 03:49:24 +0900

    test case

---
+++ src/main/java/sejeong/hellospring/service/MemberService.java
@@ -0,0 +1,40 @@
+package sejeong.hellospring.service;
+
+import sejeong.hellospring.domain.Member;
+import sejeong.hellospring.domain.MemberRepository;
+import sejeong.hellospring.domain.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+    // 회원가입
+    public Long join(Member member ) {
+        // 중복회원 검증
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> { // Optional ifPresent -> not null
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
