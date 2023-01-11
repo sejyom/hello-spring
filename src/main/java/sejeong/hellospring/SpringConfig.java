913d5e61b356afcf0c07bb36369b9d7086ce052c
sejyom <akxxkd01@gmail.com>
2023-01-11 20:44:48 +0900

    modify

--- src/main/java/sejeong/hellospring/SpringConfig.java
+++ src/main/java/sejeong/hellospring/SpringConfig.java
@@ -3,7 +3,6 @@ package sejeong.hellospring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import sejeong.hellospring.repository.JdbcMemberRepository;
 import sejeong.hellospring.repository.JdbcTemplateMemberRepository;
 import sejeong.hellospring.repository.MemberRepository;
 import sejeong.hellospring.repository.MemoryMemberRepository;
--- src/main/java/sejeong/hellospring/repository/JdbcTemplateMemberRepository.java
+++ src/main/java/sejeong/hellospring/repository/JdbcTemplateMemberRepository.java
@@ -7,8 +7,6 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import sejeong.hellospring.domain.Member;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
