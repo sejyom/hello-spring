877dfc5f8e02eed040370f96741951d56b561e91
sejyom <akxxkd01@gmail.com>
2023-01-23 22:12:21 +0900

    add Comment

--- src/test/java/sejeong/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/sejeong/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -51,7 +51,7 @@ class MemoryMemberRepositoryTest {
         repository.save(member1);
 
         Member member2 = new Member();
-        member2.setName("sejeong22");
+        member2.setName("sejeong2");
         repository.save(member2);
 
         List<Member> result = repository.findAll();
