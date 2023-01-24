f10f36962f96a60485a9ad110de9d828bace14fa
sejyom <akxxkd01@gmail.com>
2023-01-24 23:05:22 +0900

    add Comment

--- src/main/java/sejeong/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/sejeong/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository { //인터페이스가 인터페이스를 받을 땐 extends 쓰긩
 
-    //JPQL select m from Member m where m.name = ?
+    //JPQL - select m from Member m where m.name = ?
     @Override
     Optional<Member> findByName(String name);
 //    Optional<Member> findByNameAndId(String name, Long id);
